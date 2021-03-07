4709380dc593c2649cd8504b59853d3ca89a4f2c
Arho Huttunen <arho.huttunen@iki.fi>
2021-03-07 07:50:59 +0200

    Add an example of a test trying to do too much

---
+++ test-readability/src/test/java/com/arhohuttunen/relevancy/EagerTest.java
@@ -0,0 +1,33 @@
+package com.arhohuttunen.relevancy;
+
+import com.arhohuttunen.Customer;
+import com.arhohuttunen.Product;
+import com.arhohuttunen.Store;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EagerTest {
+    @Test
+    void purchaseProducts() {
+        // Arrange
+        Product paperclip = new Product("Paperclip");
+        Product printerPaper = new Product("Printer paper");
+        Store store = new Store();
+        store.addInventory(paperclip, 100);
+        store.addInventory(printerPaper, 50);
+        Customer customer = new Customer();
+        // Act
+        boolean purchaseSucceeded = customer.purchase(store, paperclip, 10);
+        // Assert
+        assertTrue(purchaseSucceeded);
+        assertEquals(90, store.getInventory(paperclip));
+        // Act
+        purchaseSucceeded = customer.purchase(store, printerPaper, 100);
+        // Assert
+        assertFalse(purchaseSucceeded);
+        assertEquals(50, store.getInventory(printerPaper));
+    }
+}
--- test-readability/src/test/java/com/arhohuttunen/relevancy/EssentialRelevant.java
+++ test-readability/src/test/java/com/arhohuttunen/relevancy/EssentialRelevant.java
@@ -1,10 +1,14 @@
 package com.arhohuttunen.relevancy;
 
+import com.arhohuttunen.Customer;
 import com.arhohuttunen.Person;
+import com.arhohuttunen.Product;
+import com.arhohuttunen.Store;
 import org.junit.jupiter.api.Test;
 
 import static com.arhohuttunen.relevancy.PersonBuilder.aPerson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EssentialRelevant {
@@ -27,5 +31,31 @@ class EssentialRelevant {
         Person person = aPerson().withFirstName("John").withLastName("Doe").build();
 
         assertEquals("John Doe", person.getFullName());
+    }
+
+    @Test
+    void purchaseSucceedsWhenEnoughInventory() {
+        Product paperclip = new Product("Paperclip");
+        Store store = new Store();
+        store.addInventory(paperclip, 100);
+        Customer customer = new Customer();
+
+        boolean purchaseSucceeded = customer.purchase(store, paperclip, 10);
+
+        assertTrue(purchaseSucceeded);
+        assertEquals(90, store.getInventory(paperclip));
+    }
+
+    @Test
+    void purchaseFailWhenNotEnoughInventory() {
+        Product printerPaper = new Product("Printer paper");
+        Store store = new Store();
+        store.addInventory(printerPaper, 50);
+        Customer customer = new Customer();
+
+        boolean purchaseSucceeded = customer.purchase(store, printerPaper, 100);
+
+        assertFalse(purchaseSucceeded);
+        assertEquals(50, store.getInventory(printerPaper));
     }
 }
